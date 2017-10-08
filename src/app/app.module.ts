81991cacb37c38622e7aeb1fa48f324bbdf503e3
shivamkejriwal <shkejriwal@paypal.com>
2017-10-08 01:13:00 -0700

    Angular setup. Inital web page stub.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-components/page-header/page-header.component';
+import { PageBodyComponent } from './page-components/page-body/page-body.component';
+import { PageFooterComponent } from './page-components/page-footer/page-footer.component';
+import { NavbarComponent } from './ui-components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    PageBodyComponent,
+    PageFooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page-components/page-body/page-body.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-body',
+  templateUrl: './page-body.component.html',
+  styleUrls: ['./page-body.component.css']
+})
+export class PageBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+      console.log('PageBodyComponent');
+  }
+
+}
