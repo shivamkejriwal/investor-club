13d92aa804404a5f8807fccc34c2595c1cea186d
Shivam Kejriwal <shivamkejriwal@gmail.com>
2017-10-29 18:43:03 -0700

    Improve math logic in utils.js

--- src/app/section-components/utils.ts
+++ src/app/section-components/utils.ts
@@ -9,22 +9,42 @@ const reduce = (list, key, alter) => {
     });
 };
 
+const combinedOperation = (eleA, eleB, op) => {
+    let res = [];
+    for(let i=0; i < eleA.length; i++) {
+        res.push(op(eleA[i],eleB[i]));
+    };
+    return res;
+}
+
 const getLastObject = (list) => {
     return (list.length > 0) ? list[list.length - 1] : {};
 };
 
+const round = (value, precision) => {
+    const multiplier = Math.pow(10, precision);
+    return Math.round(value * multiplier) / multiplier;
+}
 
-const divide = (a, b) => {
-    if (!a) return 0;
-    if (!b) return 0;
-    const result = parseFloat(a)/parseFloat(b);
-    return parseFloat(result.toFixed(8));
-};
+const divide = (a,b) => round(a/b, 2);
 
+const average = (array) => round(array.reduce((a, b) => a + b) / array.length, 2);
+
+const median = (array) => {
+    array.sort((a, b) => a - b);
+    const lowMiddle = Math.floor((array.length - 1) / 2);
+    const highMiddle = Math.ceil((array.length - 1) / 2);
+    const median = (array[lowMiddle] + array[highMiddle]) / 2;
+    return median;
+}
 const Utils = {
     reduce,
     getLastObject,
-    divide
+    round,
+    divide,
+    average,
+    median,
+    combinedOperation
 };
 
 export { Utils };
